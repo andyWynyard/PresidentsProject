25d43b58d8854cf1c2255fe1c70df4b3395e47cf
mmJava <maldojavadev@gmail.com>
2017-07-11 16:17:43 -0600

    modified

--- src/com/skilldistillery/web/PresidentsServlet.java
+++ src/com/skilldistillery/web/PresidentsServlet.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.skilldistillery.data.President;
+
 public class PresidentsServlet extends HttpServlet {
 	
 	Map<Integer, President> presMap;
